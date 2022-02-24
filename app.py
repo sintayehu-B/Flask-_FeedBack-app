a42582529fd46bb99855be2021804fc8bbbff37b
sintayehu-B <it.sintayehu.sermessa@gmail.com>
2022-02-24 15:29:02 +0300

    my first flask project. a simple web app that performs rating and email ther customer for rating also the dealer about the persons experience

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('success.html')
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='please enter required information on the fields')
+        return render_template('success.html')
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run()
